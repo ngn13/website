bcb48d789bff3c9f76f461320dd0bb3648214793
ngn <ngn@ngn.tf>
2025-04-07 04:57:29 +0300

    update the app URL env var name in API config
    Signed-off-by: ngn <ngn@ngn.tf>

--- api/routes/index.go
+++ api/routes/index.go
@@ -7,7 +7,7 @@ import (
 
 func GET_Index(c *fiber.Ctx) error {
 	conf := c.Locals("config").(*config.Type)
-	app := conf.GetURL("app_url")
+	app := conf.GetURL("app_url_clear")
 
 	// redirect to the API documentation
 	return c.Redirect(app.JoinPath("/doc/api").String())
--- api/routes/news.go
+++ api/routes/news.go
@@ -40,7 +40,7 @@ func GET_News(c *fiber.Ctx) error {
 
 	db := c.Locals("database").(*database.Type)
 	conf := c.Locals("config").(*config.Type)
-	app := conf.GetURL("app_url")
+	app := conf.GetURL("app_url_clear")
 	lang := c.Params("lang")
 
 	if lang == "" || len(lang) != 2 {
